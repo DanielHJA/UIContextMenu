df4c32925bf2762bff564238a60b5b3978e3cce1
Daniel <daniel.hjartstrom@gmail.com>
2020-03-09 10:30:30 +0100

    Initial Commit

---
+++ ContextMenuTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContextMenuTest
+//
+//  Created by Daniel Hjärtström on 2020-03-09.
+//  Copyright © 2020 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
